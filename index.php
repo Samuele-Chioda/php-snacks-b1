363dfaa1deb9b68ab8eb063ef7eb74c169e83556
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-26 12:37:22 +0200

    snack php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$partite[] = array(
+    "squadra_casa" => "Olimpia Milano",
+    "squadra_ospite" => "Cantù",
+    "punti_casa" => 55,
+    "punti_ospite" => 60
+);
+$partite[] = array(
+    "squadra_casa" => "Verona",
+    "squadra_ospite" => "Cagliari",
+    "punti_casa" => 45,
+    "punti_ospite" => 40
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div>
+            <?php foreach ($partite as $partita) {
+                echo $partita["squadra_casa"] . " - " . $partita["squadra_ospite"] . " | " . $partita["punti_casa"] . " - " . $partita["punti_ospite"] . "<br>";
+            } 
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
