911a54235937c2563ed09fb91933882ef00ee731
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-26 16:20:31 +0200

    snack4 ciclo for e stampa in pagina di 15 numeri casuali

--- snack4.php
+++ snack4.php
@@ -1,17 +1,33 @@
 <?php
+// dichiaro valori di min e max
+$min = 0;
+$max = 100;
+// creo array vuoto per contenere i 15 numeri generati
+$numbersArray = [];
+
+// ciclo for per iterare 15 volte 
+for ($i = 0; $i < 15; $i++) {
+    $casualNumber = rand($min, $max);
+    echo $casualNumber;
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <main>
-        
+        <div>
+
+        </div>
     </main>
 </body>
+
 </html>
