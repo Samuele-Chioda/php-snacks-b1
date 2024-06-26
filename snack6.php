b11cbcee13e88b902a11f3a410497b784308167d
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-26 17:21:36 +0200

    snack6 aggiunta array

--- snack6.php
+++ snack6.php
@@ -1,17 +1,43 @@
 <?php
 
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <main>
-        
+
     </main>
 </body>
+
 </html>
