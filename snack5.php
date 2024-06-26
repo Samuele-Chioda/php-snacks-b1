e82b8ec3c7a887c5c90e59c6efcdd8f0cd60d5cf
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-26 17:16:36 +0200

    aggiunta metodo explode e ciclo foreach

--- snack5.php
+++ snack5.php
@@ -2,6 +2,12 @@
 
 $paragrafo = 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.';
 
+// metodo explode che prende come primo valore l'elememnto che fa da divisore e come secondo elemento la variabile da esplodere
+$sezioni = explode('.', $paragrafo);
+// ciclo foreach che itera su sezioni e per ogni punto stampa un paragrafo
+foreach ($sezioni as $sezione) {
+echo '<p>' . $sezione . '</p>';
+};
 
 ?>
 
