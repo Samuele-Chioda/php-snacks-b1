ed491ba12f0475e888f6823f5b8648927e303e1e
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-26 18:04:52 +0200

    snack 6 completo

--- snack6.php
+++ snack6.php
@@ -1,5 +1,5 @@
 <?php
-
+// array di array
 $db = [
     'teachers' => [
         [
@@ -36,8 +36,47 @@ $db = [
 
 <body>
     <main>
-
+    <div class="grigio">
+            <ul>
+                <!-- ciclo foreach che itera teachers di array $db e crea elemento $teacher che viene inserito in li specificandone il valore che voglio -->
+                <?php foreach ($db['teachers'] as $teacher) { ?>
+                    <li>
+                        <?php echo $teacher['name'] . ' ' . $teacher['lastname']; ?>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+        <div class="verde">
+            <ul>
+                <?php foreach ($db['pm'] as $pm) { ?>
+                    <li>
+                        <?php echo $pm['name'] . ' ' . $pm['lastname']; ?>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
     </main>
 </body>
 
 </html>
+
+<style>
+    .grigio{
+        width: 100%;
+        height: 200px;
+        background-color: grey;
+        margin-bottom: 5rem;
+    }
+    .verde{
+        width: 100%;
+        height: 200px;
+        background-color: green;
+    }
+    li{
+        font-size: 3rem;
+        font-weight: bolder;
+        text-align: center;
+        margin-bottom: 3rem;
+    }
+
+</style>
